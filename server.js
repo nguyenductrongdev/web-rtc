bafac64d8a28b1cad9dcc42effb9cb045b264112
ndtrong <nguyenductrong12a1@gmail.com>
2024-05-14 22:37:38 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+
+    socket.on('offer', (data) => {
+        socket.broadcast.emit('offer', data);
+    });
+
+    socket.on('answer', (data) => {
+        socket.broadcast.emit('answer', data);
+    });
+
+    socket.on('candidate', (data) => {
+        socket.broadcast.emit('candidate', data);
+    });
+});
+
+server.listen(8080, () => {
+    console.log('Server listening on port 8080');
+});
